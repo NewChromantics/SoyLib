35d135c522e162c496c250d9b703d0347ec94cb0
Graham Reeves <graham@grahamreeves.com>
2013-03-13 12:56:28 +0000

    Added ofIsValidFloat

--- src/SoyMath.h
+++ src/SoyMath.h
@@ -67,4 +67,74 @@ inline float ofAngleFromNormal(const vec2f& Normal)
 	return SCREEN_UP2.angle( n );
 }
 
+inline bool ofIsValidFloat(double Value)
+{
+	if ( Value != Value )
+		return false;
+	if ( _isnan(Value) )
+		return false;
+	return true;
+}
 
+inline bool ofIsValidFloat(float Value)
+{
+	//	standard way of detecting invalid floats
+	if ( Value != Value )
+		return false;
+
+#if (_MSC_VER) || defined (TARGET_ANDROID)
+#ifndef isnan
+#define isnan(a) ((a) != (a))
+#endif
+	if ( isnan(Value) )
+		return false;
+#else
+	if ( std::isnan(Value) )
+		return false;
+#endif
+	
+	//	IEEE test
+	if ( _isnan(Value) )
+		return false;
+
+	return true;
+}
+
+inline bool ofIsValidFloat(const ofVec2f& Value)
+{
+	return ofIsValidFloat(Value.x) && 
+			ofIsValidFloat(Value.y);
+}
+
+inline bool ofIsValidFloat(const ofVec3f& Value)
+{
+	return ofIsValidFloat(Value.x) && 
+			ofIsValidFloat(Value.y) && 
+			ofIsValidFloat(Value.z);
+}
+
+inline bool ofIsValidFloat(const ofVec4f& Value)
+{
+	return ofIsValidFloat(Value.x) && 
+			ofIsValidFloat(Value.y) && 
+			ofIsValidFloat(Value.z) && 
+			ofIsValidFloat(Value.w);
+}
+
+inline bool ofIsValidFloat(const float* Values,int Count)
+{
+	for ( int i=0;	i<Count;	i++ )
+	{
+		if ( !ofIsValidFloat( Values[i] ) )
+			return false;
+	}
+	return true;
+}
+
+inline bool ofIsValidFloat(const ofMatrix4x4& Value)
+{
+	return ofIsValidFloat( Value.getRowAsVec4f(0) ) && 
+			ofIsValidFloat( Value.getRowAsVec4f(1) ) && 
+			ofIsValidFloat( Value.getRowAsVec4f(2) ) && 
+			ofIsValidFloat( Value.getRowAsVec4f(3) );
+}
