8b32dcf9dce845becdbbf93e1f02c0fc213959c4
Graham Reeves <graham@grahamreeves.com>
2013-06-16 14:05:01 +0100

    Added a 4-point ofBezier()

--- src/SoyRender.h
+++ src/SoyRender.h
@@ -65,3 +65,9 @@ void ofDrawTriangles(const ArrayBridge<ofShapeTriangle2>& Triangles,ofTexture& T
 
 class ofShapeCapsule2;
 void ofCapsule(const ofShapeCapsule2& Capsule,float z=0.f,bool DrawCenterLine=false);
+
+inline void ofBezier(ofPoint p0,ofPoint p1,ofPoint p2,ofPoint p3)
+{
+	ofBezier( p0.x, p0.y, p0.z, p1.x, p1.y, p1.z, p2.x, p2.y, p2.z, p3.x, p3.y, p3.z );
+}
+
