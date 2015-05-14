acda703ae7ab668c1df51376e41a93a0e08c5596
Graham Reeves <graham@grahamreeves.com>
2015-05-14 17:12:25 +0100

    omitted line feed from windows platform errors

--- src/SoyTypes.cpp
+++ src/SoyTypes.cpp
@@ -162,6 +162,12 @@ std::string Soy::Platform::GetErrorString(int Error)
 	std::string result(lpMsgStr, lpMsgStr+bufLen);
 
 	LocalFree(lpMsgBuf);
+
+	//	gr: trim off line feeds
+	BufferArray<char, 2> LineFeeds;
+	LineFeeds.PushBack('\n');
+	LineFeeds.PushBack('\r');
+	Soy::StringTrimRight(result, GetArrayBridge(LineFeeds));
 	return result;
 #else
 	if ( Error == 0 )
