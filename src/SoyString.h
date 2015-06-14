38d1cf022e4d1c4bcbfc1a1894ff83171c2476dc
Graham Reeves <graham@grahamreeves.com>
2015-06-15 00:15:32 +0100

    added matrix & vector string << and >> operators

--- src/SoyString.h
+++ src/SoyString.h
@@ -6,11 +6,28 @@
 #include <sstream>
 #include "memheap.hpp"
 #include "SoyAssert.h"
+#include "SoyMath.h"	//	for vec <-> string funcs
+
 
 //	foraward declarations
 template<typename TYPE>
 class ArrayBridge;
 
+
+namespace Soy
+{
+	//	Soy::lf
+	//	like std::endl without the implicit flush. I prefer this over \n's
+	template <class _CharT, class _Traits>
+	inline _LIBCPP_INLINE_VISIBILITY std::basic_ostream<_CharT, _Traits>& lf(std::basic_ostream<_CharT, _Traits>& __os)
+	{
+		__os.put(__os.widen('\n'));
+		return __os;
+	}
+	
+	//	delineators for strings. First is used for output
+	static const char* VecNXDelins = ",x";
+};
 
 //	string that uses a prmem::Heap
 //	not compatible with std::string, but maybe we can do an easy conversion func
@@ -64,6 +81,10 @@ namespace Soy
 	bool		StringToType(TYPE& Out,const std::string& String);
 	template<typename TYPE>
 	TYPE		StringToType(const std::string& String,const TYPE& Default);
+
+	//	max size of vector (ie. buffer array/remote array) dictates expected size
+	template<typename TYPE>
+	inline bool	StringParseVecNx(const std::string& String,ArrayBridge<TYPE>&& Vector);
 };
 
 #if defined(__OBJC__)
@@ -121,3 +142,141 @@ inline std::string Soy::StringJoin(const ArrayBridge<TYPE>& Elements,const std::
 	}
 	return Stream.str();
 }
+
+
+template<typename TYPE>
+inline bool Soy::StringParseVecNx(const std::string& instr,ArrayBridge<TYPE>&& Components)
+{
+	auto Append = [&Components](const std::string& Valuestr)
+	{
+		TYPE Value;
+		if ( !Soy::StringToType( Value, Valuestr ) )
+			return false;
+		if ( Components.IsFull() )
+			return false;
+		Components.PushBack( Value );
+		return true;
+	};
+	if ( !Soy::StringSplitByMatches( Append, instr, Soy::VecNXDelins, false ) )
+		return false;
+	
+	//	didn;t find enough components
+	if ( !Components.IsFull() )
+		return false;
+
+	return true;
+}
+
+
+//	gr: in string rather than math to reduce math's dependancies.
+//		this header already requires array's so here it is
+template<typename TYPE>
+inline std::istream& operator>>(std::istream &in,vec2x<TYPE> &out)
+{
+	std::string instr;
+	in >> instr;
+	if ( in.fail() )
+		return in;
+	
+	BufferArray<TYPE,2> VecArray;
+	if ( !Soy::StringParseVecNx( instr, GetArrayBridge( VecArray ) ) )
+	{
+		in.setstate( std::ios::failbit );
+		return in;
+	}
+
+	out.x = VecArray[0];
+	out.y = VecArray[1];
+	
+	return in;
+}
+
+
+
+template<typename TYPE>
+inline std::istream& operator>>(std::istream &in,vec3x<TYPE> &out)
+{
+	std::string instr;
+	in >> instr;
+	if ( in.fail() )
+		return in;
+	
+	BufferArray<TYPE,3> VecArray;
+	if ( !Soy::StringParseVecNx( instr, GetArrayBridge( VecArray ) ) )
+	{
+		in.setstate( std::ios::failbit );
+		return in;
+	}
+	
+	out.x = VecArray[0];
+	out.y = VecArray[1];
+	out.z = VecArray[2];
+	
+	return in;
+}
+
+template<typename TYPE>
+inline std::ostream& operator<<(std::ostream &out,const vec2x<TYPE> &in)
+{
+	out << in.x << Soy::VecNXDelins[0] << in.y;
+	return out;
+}
+
+template<typename TYPE>
+inline std::ostream& operator<<(std::ostream &out,const Soy::Matrix2x1 &in)
+{
+	out << in.x() << Soy::VecNXDelins[0] << in.y();
+	return out;
+}
+
+template<typename TYPE>
+inline std::ostream& operator<<(std::ostream &out,const vec3x<TYPE> &in)
+{
+	out << in.x << Soy::VecNXDelins[0] << in.y << Soy::VecNXDelins[0] << in.z;
+	return out;
+}
+
+template<typename TYPE>
+inline std::ostream& operator<<(std::ostream &out,const Soy::Matrix3x1 &in)
+{
+	out << in.x() << Soy::VecNXDelins[0] << in.y() << Soy::VecNXDelins[0] << in.z();
+	return out;
+}
+
+template<typename TYPE>
+inline std::ostream& operator<<(std::ostream &out,const vec4x<TYPE> &in)
+{
+	out << in.x << Soy::VecNXDelins[0] << in.y << Soy::VecNXDelins[0] << in.z << Soy::VecNXDelins[0] << in.w;
+	return out;
+}
+
+template<typename TYPE>
+inline std::ostream& operator<<(std::ostream &out,const Soy::Matrix4x1 &in)
+{
+	out << in.x() << Soy::VecNXDelins[0] << in.y() << Soy::VecNXDelins[0] << in.z() << Soy::VecNXDelins[0] << in.w();
+	return out;
+}
+
+template<typename TYPE>
+inline std::ostream& operator<<(std::ostream &out,const Soy::Matrix4x4 &in)
+{
+	for ( int i=0;	i<in.kElements;	i++ )
+	{
+		if ( i > 0 )
+			out << Soy::VecNXDelins[0];
+		out << in[i];
+	}
+	
+	return out;
+}
+
+template<typename TYPE>
+inline std::ostream& operator<<(std::ostream &out,const float4x4&in)
+{
+	out << in.rows[0] << Soy::VecNXDelins[0]
+	<< in.rows[1] << Soy::VecNXDelins[0]
+	<< in.rows[2] << Soy::VecNXDelins[0]
+	<< in.rows[3];
+	return out;
+}
+
