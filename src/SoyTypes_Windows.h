c2c88adfa575e7c7b23b790074b7597e42b3e6fb
Graham Reeves <graham@grahamreeves.com>
2015-07-07 16:54:47 +0100

    Put the dx-style auto release smart pointer into windows types

--- src/SoyTypes_Windows.h
+++ src/SoyTypes_Windows.h
@@ -32,3 +32,51 @@ typedef unsigned __int32	uint32;
 typedef signed __int64		int64;
 typedef unsigned __int64	uint64;
 typedef SSIZE_T				ssize_t;
+
+
+
+
+//	this is for directx really but so many things have a Release() it can probably move
+template<typename TYPE>
+class AutoReleasePtr
+{
+public:
+	AutoReleasePtr() :
+	mObject	( nullptr )
+	{
+	}
+	AutoReleasePtr(TYPE* Object,bool AddRef) :
+	mObject	( nullptr )
+	{
+		Set( Object, AddRef );
+	}
+	AutoReleasePtr(const TAutoRelease& that) :
+	mObject	( nullptr )
+	{
+		Set( that.mObject, true );
+	}
+	~AutoReleasePtr()		{	Release();	}
+	
+	TYPE*		operator ->()	{	return mObject;	}
+	operator	TYPE*()			{	return mObject;	}
+	
+	void		Set(TYPE* Object,bool AddRef)
+	{
+		Release();
+		mObject = Object;
+		if ( AddRef && mObject )
+			mObject->AddRef();
+	}
+	
+	void		Release()
+	{
+		if ( mObject )
+		{
+			mObject->Release();
+			mObject = nullptr;
+		}
+	}
+	
+public:
+	TYPE*	mObject;
+};
