1dbb4496fb69ced2c340ec249fd75881d61af8cd
Graham Reeves <graham@grahamreeves.com>
2015-07-13 20:49:58 +0100

    Added TFileWatch which sends an event when a file is changed

--- src/SoyFilesytem.cpp
+++ src/SoyFilesytem.cpp
@@ -1,96 +1,107 @@
-#include "SoyFilesytem.h"
+#include "SoyFilesystem.h"
 #include "SoyDebug.h"
 
-
-SoyFilesystem::Timestamp::Timestamp()
-{
 #if defined(TARGET_OSX)
-	memset( &mTime, 0, sizeof(mTime) );
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+#include <CoreServices/CoreServices.h>
 #endif
-}
 
 
-bool SoyFilesystem::Timestamp::IsValid() const
+SoyTime Soy::GetFileTimestamp(const std::string& Filename)
 {
-	return (*this) != Timestamp();
-}
-
-bool SoyFilesystem::Timestamp::operator==(const Timestamp& that) const
-{
-	auto cmp = memcmp( &this->mTime, &that.mTime, sizeof(mTime) );
-	return cmp == 0;
-}
-
-
-SoyFilesystem::File::File(Path Path,std::string Filename)
-{
-	//	extract path from filename...
-	mName += Path.GetFullPath();
-	mName += Filename;
-}
-
-
-SoyFilesystem::File::File(std::string Filename) :
-	mName	( Filename )
-{
-}
-
-std::string SoyFilesystem::File::GetFullPathFilename() const 
-{
-	auto Path = GetPath();
-	std::stringstream FullPathFilename;
-	FullPathFilename << Path.GetFullPath();
-	FullPathFilename << mName;
-	return FullPathFilename.str();
-}
-
-SoyFilesystem::Path SoyFilesystem::File::GetPath() const
-{
-	//	todo
-	SoyFilesystem::Path Path("./");
-	return Path;
-}
-
-
-SoyFilesystem::Timestamp SoyFilesystem::File::GetModified()
-{
+	/*
 #if defined(TARGET_OSX)
+	struct tm Time;
+	memset( &Time, 0, sizeof(Time) );
+
 	struct stat attrib;			// create a file attribute structure
-	auto Filename = GetFullPathFilename();
 	auto Result = stat( Filename.c_str(), &attrib);		// get the attributes of afile.txt
 	if ( Result != 0 )
-	{
-		std::Debug << "Error getting filestamp(stat) of " << Filename << ": " << Soy::Platform::GetLastErrorString() << std::endl;
-		return Timestamp();
-	}
-	
-	SoyFilesystem::Timestamp Time;
-				// create a time structure
-	if ( !gmtime_r( &attrib.st_mtime, &Time.mTime ) )
-	{
-		std::Debug << "Error getting filestamp(gmtime) of " << Filename << ": " << Soy::Platform::GetLastErrorString() << std::endl;
-		return Timestamp();
-	}
+		return SoyTime();
 		
-	return Time;
-#else
-	return Timestamp();
+	// create a time structure
+	if ( !gmtime_r( &attrib.st_mtime, &Time ) )
+		return SoyTime();
+	
+	//return Time.
+#endif
+	 */
+	throw new Soy::AssertException( std::string(__func__)+" not implemented");
+	return SoyTime();
+}
+
+
+
+void mycallback(
+    ConstFSEventStreamRef streamRef,
+    void *clientCallBackInfo,
+    size_t numEvents,
+    void *eventPaths,
+    const FSEventStreamEventFlags eventFlags[],
+    const FSEventStreamEventId eventIds[])
+{
+	auto& FileWatch = *reinterpret_cast<Soy::TFileWatch*>( clientCallBackInfo );
+	char **paths = reinterpret_cast<char **>(eventPaths);
+	
+	for ( int e=0;	e<numEvents;	e++ )
+	{
+		std::string Filename( paths[e] );
+		const FSEventStreamEventFlags& EventFlags( eventFlags[e] );
+		const FSEventStreamEventId EventIds( eventIds[e] );
+		
+		FileWatch.mOnChanged.OnTriggered( Filename );
+	}
+}
+
+
+Soy::TFileWatch::TFileWatch(const std::string& Filename)
+{
+	//	debug callback
+	auto DebugOnChanged = [](const std::string& Filename)
+	{
+		std::Debug << Filename << " changed" << std::endl;
+	};
+	mOnChanged.AddListener( DebugOnChanged );
+	
+#if defined(TARGET_OSX)
+	
+	//std::string Filename("/Volumes/Code/PopTrack/");
+	mPathString.Retain( CFStringCreateWithCString( nullptr, Filename.c_str(), kCFStringEncodingUTF8 ) );
+	CFArrayRef pathsToWatch = CFArrayCreate(NULL, (const void **)&mPathString.mObject, 1, NULL);
+	
+	CFAbsoluteTime latency = 1.0; /* Latency in seconds */
+	FSEventStreamContext Context = {0, this, NULL, NULL, NULL};
+	
+	/* Create the stream, passing in a callback */
+	mStream.mObject = FSEventStreamCreate(NULL,
+										  &mycallback,
+										  &Context,
+										  pathsToWatch,
+										  kFSEventStreamEventIdSinceNow,
+										  latency,
+										  kFSEventStreamCreateFlagFileEvents
+										  );
+	
+	//	add to new runloop
+	FSEventStreamScheduleWithRunLoop( mStream.mObject, CFRunLoopGetMain(), kCFRunLoopDefaultMode );
+	FSEventStreamStart( mStream.mObject );
 #endif
 }
 
 
-
-SoyFilesystem::Path::Path(std::string Path) :
-	mName	( Path )
+Soy::TFileWatch::~TFileWatch()
 {
-	
+#if defined(TARGET_OSX)
+	if ( mStream )
+	{
+		FSEventStreamUnscheduleFromRunLoop( mStream.mObject, CFRunLoopGetMain(), kCFRunLoopDefaultMode );
+		FSEventStreamStop( mStream.mObject );
+		//	make sure all queued events are gone
+		FSEventStreamFlushSync( mStream.mObject );
+		mStream.Release();
+	}
+#endif
 }
-
-
-std::string SoyFilesystem::Path::GetFullPath() const
-{
-	//	todo
-	return mName;
-}
-
 
--- src/SoyFilesytem.h
+++ src/SoyFilesytem.h
@@ -1,76 +1,33 @@
 #pragma once
 
-#include "SoyTypes.h"
+#include "SoyEvent.h"
+#include "SoyTime.h"
+#include <CoreServices/CoreServices.h>
 
+namespace Soy
+{
+	SoyTime		GetFileTimestamp(const std::string& Filename);
+	
+	class TFileWatch;
+};
+
+
+namespace Soy
+{
+	class TFileWatch;
+};
+
+class Soy::TFileWatch
+{
+public:
+	TFileWatch(const std::string& Filename);
+	~TFileWatch();
+	
+	SoyEvent<const std::string>	mOnChanged;
+	
 #if defined(TARGET_OSX)
-	#include <sys/stat.h>
-	#include <unistd.h>
-	#include <time.h>
-#endif
-
-
-namespace SoyFilesystem
-{
-	class Timestamp;	//	file timestamp
-	class File;			//
-	class Path;			//	directory
-};
-
-
-
-class SoyFilesystem::Path
-{
-public:
-	Path(std::string Path);
-	
-	bool		IsValid()		{	return !mName.empty();	}
-	bool		Exists();
-	bool		IsFullPath() const;
-	std::string	GetFullPath() const;
-	std::string	GetPath() const;
-	
-private:
-	std::string	mName;
-};
-
-
-
-class SoyFilesystem::File
-{
-public:
-	File(Path Path,std::string Filename);
-	File(std::string Filename="");
-	
-	std::string	GetFilename() const	{	return mName;	}
-	std::string	GetFullPathFilename() const;
-	bool		IsValid()			{	return !mName.empty();	}
-	bool		Exists();
-	Timestamp	GetModified();
-	Path		GetPath() const;
-	bool		HasAbsolutePath() const;
-
-	inline bool	operator==(const File& that) const	{	return this->GetFullPathFilename() == that.GetFullPathFilename();	}
-	
-private:
-	std::string	mName;
-};
-
-
-
-
-class SoyFilesystem::Timestamp
-{
-public:
-	Timestamp();
-	
-	bool		IsValid() const;
-	
-	bool		operator==(const Timestamp& that) const;
-	bool		operator!=(const Timestamp& that) const		{	return !(*this == that);	}
-	
-public:
-#if defined(TARGET_OSX)
-	struct tm	mTime;
+	CFPtr<CFStringRef>		mPathString;
+	CFPtr<FSEventStreamRef>	mStream;
 #endif
 };
 
