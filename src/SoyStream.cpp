216f57984019cd18c4b4ba89c32a73f5db9c2193
Graham Reeves <graham@grahamreeves.com>
2015-09-16 21:57:15 +0100

    ChannelBuffer moved to soy as StreamBuffer. With read & write base threads

--- src/SoyStream.cpp
+++ src/SoyStream.cpp
@@ -1,22 +1,378 @@
-#include "ofxSoylent.h"
 #include "SoyStream.h"
-#include "ofxXmlSettings.h"
+#include <regex>
+#include "RemoteArray.h"
+#include "SoyProtocol.h"
 
 
-bool SoyStream::TFile::Write(const ofxXmlSettings& xml)
+
+bool TStreamBuffer::Pop(std::string Pattern,ArrayBridge<std::string>& Parts)
 {
-	BufferString<MAX_PATH> Filename = GetFilename();
-	auto& xmlmutable = const_cast<ofxXmlSettings&>( xml );
-	return xmlmutable.saveFile( Filename.c_str() );
-}
-
-bool SoyStream::TFile::Read(ofxXmlSettings& xml)
-{
-	BufferString<MAX_PATH> Filename = GetFilename();
-	if ( !xml.loadFile( Filename.c_str() ) )
+	std::lock_guard<std::mutex>	Lock( mLock );
+	
+	//assert( Pattern.empty() );
+	if ( Pattern.empty() )
 		return false;
-	//	mark last-modified
+	
+	//	gr: because we NEED to match the start of the buffer, the pattern MUST beging with ^(start of string)
+	if ( Pattern[0] != '^' )
+	{
+		std::Debug << __FUNCTION__ << " regular expression pattern MUST start with ^" << std::endl;
+		//assert(Pattern[0] == '^');
+		return false;
+	}
+	
+	//	pull out data to test against
+	size_t Lookahead = std::min<size_t>( 100, mData.GetSize() );
+	std::stringstream Data;
+	for ( size_t i=0;	i<Lookahead;	i++ )
+	{
+		Data << mData[i];
+	}
+	std::string DataString = Data.str();
+	std::smatch Match;
+	
+	//	execute regex
+	//	gr: gotta use search as we might be testing against MORE than just what we want to match
+	if ( !std::regex_search( DataString, Match, std::regex(Pattern) ) )
+		return false;
+	
+	//	matched. pop off the data
+	auto Length = Match[0].str().length();
+	mData.RemoveBlock( 0, Length );
+	
+	//	get the parts into the result
+	for ( int i=1;	i<Match.size();	i++ )
+	{
+		Parts.PushBack( Match[i].str() );
+	}
+	
 	return true;
 }
+
+bool TStreamBuffer::PopAnyMatch(const ArrayInterface<char>&& DelimAny,ArrayBridge<char>& Data,bool KeepDelim)
+{
+	std::lock_guard<std::mutex>	Lock( mLock );
+
+	//	delim is empty??
+	if ( !Soy::Assert( !DelimAny.IsEmpty(), "no deliminators sepecified" ) )
+		return false;
+	
+	//	search for match
+	for ( int a=0;	a<mData.GetSize();	a++ )
+	{
+		bool Match = false;
+		for ( int d=0;	d<DelimAny.GetSize();	d++ )
+			Match |= (DelimAny[d] == mData[a]);
+		if ( !Match )
+			continue;
+		
+		//	found match!
+		int PopLength = a + 1;
+		int OutputLength = KeepDelim ? PopLength : a;
+		assert( OutputLength <= PopLength );
+		if ( !Pop( PopLength, Data ) )
+		{
+			//	unexpected!
+			Soy::Assert(false, "Unexpectedly failed to pop data we thought we had");
+			return false;
+		}
+		return true;
+	}
+	
+	//	no match
+	return false;
+}
+
+
+bool TStreamBuffer::Pop(const ArrayBridge<char>&& Delim,ArrayBridge<char>&& Data,bool KeepDelim)
+{
+	std::lock_guard<std::mutex>	Lock( mLock );
+	
+	//	delim is empty??
+	if ( !Soy::Assert( !Delim.IsEmpty(), "no deliminator sepecified" ) )
+		return false;
+	
+	//	search for match
+	auto MaxSearch = size_cast<ssize_t>(mData.GetSize()-Delim.GetSize());
+	for ( int a=0;	a<MaxSearch;	a++ )
+	{
+		bool Match = memcmp( Delim.GetArray(), &mData[a], Delim.GetDataSize() )==0;
+		if ( !Match )
+			continue;
+		
+		//	found match!
+		auto PopLength = a + (KeepDelim ? Delim.GetDataSize() : 0);
+		//auto OldSize = mData.GetSize();
+		if ( !Pop( PopLength, Data ) )
+		{
+			//	unexpected!
+			Soy::Assert(false, "Unexpectedly failed to pop data we thought we had");
+			return false;
+		}
+		
+		//	remove the delim from next search
+		if ( !KeepDelim )
+			Pop( Delim.GetDataSize() );
+		
+		return true;
+	}
+	
+	//	no match
+	return false;
+}
+
+
+bool TStreamBuffer::Pop(const std::string Delim,std::string& Data,bool KeepDelim)
+{
+	std::lock_guard<std::mutex>	Lock( mLock );
+	
+	//	search array
+	auto DelimLength = Delim.length();
+	auto DelimArray = GetRemoteArray( Delim.c_str(), DelimLength, DelimLength );
+	
+	//	delim is empty??
+	assert( !DelimArray.IsEmpty() );
+	if ( DelimArray.IsEmpty() )
+		return false;
+	
+	//	search for match
+	auto MaxSearch = size_cast<ssize_t>(mData.GetSize()-DelimArray.GetSize());
+	for ( int a=0;	a<=MaxSearch;	a++ )
+	{
+		bool Match = memcmp( &mData[a], DelimArray.GetArray(), DelimArray.GetDataSize() )==0;
+		if ( !Match )
+			continue;
+		
+		//	found match!
+		auto PopLength = a + DelimArray.GetSize();
+		auto OutputLength = KeepDelim ? PopLength : a;
+		assert( OutputLength <= PopLength );
+		Array<char> Output;
+		auto OutputBridge = GetArrayBridge( Output );
+		if ( !Pop( PopLength, OutputBridge ) )
+		{
+			//	unexpected!
+			assert(false);
+			return false;
+		}
+		//	copy into string
+		std::stringstream OutputString;
+		for ( int i=0;	i<OutputLength;	i++ )
+			OutputString << Output[i];
+		Data = OutputString.str();
+		return true;
+	}
+	
+	//	no match
+	return false;
+}
+
+
+bool TStreamBuffer::Pop(size_t Length)
+{
+	std::lock_guard<std::mutex>	Lock( mLock );
+	
+	//	enough data ready?
+	if ( mData.GetDataSize() < Length )
+		return false;
+	
+	//	remove it
+	mData.RemoveBlock( 0, Length );
+	return true;
+}
+
+bool TStreamBuffer::Pop(size_t Length,ArrayBridge<char>& Data)
+{
+	std::lock_guard<std::mutex>	Lock( mLock );
+	
+	//	enough data ready?
+	if ( mData.GetDataSize() < Length )
+		return false;
+	
+	//	push back a sub section
+	auto DataSection = GetRemoteArray( mData.GetArray(), Length );
+	if ( !Data.PushBackArray( DataSection ) )
+		return false;
+	
+	//	remove it now it's copied
+	mData.RemoveBlock( 0, Length );
+	return true;
+}
+
+bool TStreamBuffer::Push(const std::string& Data)
+{
+	std::lock_guard<std::mutex>	Lock( mLock );
+	
+	auto DataLength = Data.length();
+	auto DataArray = GetRemoteArray( Data.c_str(), DataLength );
+	mData.PushBackArray( DataArray );
+	
+	//	gr: needs to be outside of lock?
+	bool Dummy;
+	mOnDataPushed.OnTriggered(Dummy);
+	return true;
+}
+
+bool TStreamBuffer::Push(const ArrayBridge<char>& Data)
+{
+	if ( Data.IsEmpty() )
+		return true;
+	
+	std::lock_guard<std::mutex>	Lock( mLock );
+	
+	mData.PushBackArray( Data );
+	
+	//	gr: needs to be outside of lock?
+	bool Dummy;
+	mOnDataPushed.OnTriggered(Dummy);
+	return true;
+}
+
+bool TStreamBuffer::UnPop(const ArrayBridge<char>& Data)
+{
+	std::lock_guard<std::mutex>	Lock( mLock );
+	
+	auto mDataBridge = GetArrayBridge( mData );
+	if ( !mDataBridge.InsertArray( Data, 0 ) )
+		return false;
+	
+	//	gr: needs to be outside of lock?
+	bool Dummy;
+	mOnDataPushed.OnTriggered(Dummy);
+	return true;
+}
+
+
+bool TStreamBuffer::UnPop(const std::string& String)
+{
+	Array<char> Data;
+	auto DataBridge = GetArrayBridge( Data );
+	Soy::StringToArray( String, DataBridge );
+	
+	//	gr: needs to be outside of lock?
+	bool Dummy;
+	mOnDataPushed.OnTriggered(Dummy);
+	return UnPop( DataBridge );
+}
+
+bool TStreamBuffer::PeekBack(ArrayBridge<char> &&Data)
+{
+	std::lock_guard<std::mutex>	Lock( mLock );
+	
+	if ( mData.GetSize() < Data.GetSize() )
+		return false;
+	
+	auto DataTail = GetRemoteArray( &mData[mData.GetSize()-Data.GetSize()], Data.GetSize() );
+	Data.Copy( DataTail );
+	
+	return true;
+}
+
+
+
+
+TStreamReader::TStreamReader(const std::string& Name) :
+	SoyWorkerThread	( Name, SoyWorkerWaitMode::Sleep )
+{
+	
+}
+
+
+bool TStreamReader::Iteration()
+{
+	//	read next chunk
+	Read();
+	
+	//	nothing to do
+	if ( mReadBuffer.IsEmpty() )
+		return true;
+	
+	//	alloc protocol instance to process
+	if ( !mCurrentProtocol )
+	{
+		mCurrentProtocol = AllocProtocol();
+		if ( !mCurrentProtocol )
+			return true;
+	}
+	
+	//	process
+	auto DecodeResult = mCurrentProtocol->Decode( mReadBuffer );
+	
+	switch ( DecodeResult )
+	{
+			//	waiting for more data, just let thread re-iterate
+		case TProtocolState::Waiting:
+			return true;
+			
+			//	fall through
+		case TProtocolState::Finished:
+			break;
+			
+			//	release current and re-iterate
+		default:
+			std::Debug << "Unhandled TProtocolState: " << DecodeResult << " ignoring." << std::endl;
+		case TProtocolState::Ignore:
+			mCurrentProtocol.reset();
+			return true;
+	}
+	
+	//	notify (with shared ptr so data can be cheaply saved)
+	mOnDataRecieved.OnTriggered( mCurrentProtocol );
+	
+	//	dealloc for next
+	mCurrentProtocol.reset();
+	return true;
+}
+
+
+
+
+
+
+TStreamWriter::TStreamWriter(const std::string& Name) :
+	SoyWorkerThread	( Name, SoyWorkerWaitMode::Sleep )
+{
+	
+}
+
+
+bool TStreamWriter::Iteration()
+{
+	if ( mQueue.IsEmpty() )
+		return true;
+	
+	//	pop next
+	mQueueLock.lock();
+	auto Data = mQueue[0];
+	mQueue.RemoveBlock(0,1);
+	mQueueLock.unlock();
+	
+	//	turn it into data
+	//	todo: have two threads, one writing the Buffer, and one that pops the queue and just keeps pushing data
+	//		onto the buffer. That way we can encode & write at the same time and have infinite write protocols!
+	TStreamBuffer Buffer;
+	try
+	{
+		Data->Encode( Buffer );
+	}
+	catch (std::exception& e)
+	{
+		std::Debug << "Failed to write buffer from protocol: " << e.what() << std::endl;
+		return true;
+	}
+	
+	//	now write
+	Write( Buffer );
+	return true;
+}
+
+void TStreamWriter::Push(std::shared_ptr<Soy::TWriteProtocol>& Data)
+{
+	std::lock_guard<std::mutex> Lock( mQueueLock );
+	mQueue.PushBack( Data );
+	Wake();
+}
+
+
 
 
--- src/SoyStream.h
+++ src/SoyStream.h
@@ -1,50 +1,97 @@
 #pragma once
 
-#include "String.hpp"
+
+#include "SoyEvent.h"
+#include "SoyArray.h"
+#include "HeapArray.hpp"
+#include "SoyThread.h"
 
 
-namespace SoyStream
+namespace Soy
 {
-	class TStream;
-	class TFile;
-};
-
-class ofxXmlSettings;
-
-
-
-
-
-class SoyStream::TStream
-{
-public:
-	virtual ~TStream()	{}
-
-	virtual bool	IsValid()=0;
-	virtual bool	Write(const ofxXmlSettings& xml)=0;
-	virtual bool	HasChanged()=0;
-	virtual bool	Read(ofxXmlSettings& xml)=0;
+	class TWriteProtocol;
+	class TReadProtocol;
 };
 
 
-class SoyStream::TFile : public SoyStream::TStream
+//	try and integrate this more closely with std streams
+//	but for now, it's a buffer of data we've (probbaly) read in, and want to probe it in various ways
+class TStreamBuffer
 {
 public:
-	TFile(const char* Filename) :
-		mFilename	( Filename )
-	{
-	}
-
-	virtual bool	IsValid()							{	return true;	}
-	virtual bool	Write(const ofxXmlSettings& xml);
-	virtual bool	HasChanged()						{	return true;	}
-	virtual bool	Read(ofxXmlSettings& xml);
-
-protected:
-	virtual BufferString<MAX_PATH>	GetFilename()		{	return mFilename;	}
-
-protected:
-	BufferString<MAX_PATH>	mFilename;
+	//	gr: turn these into some kinda "binary reg exp"
+	bool		PopAnyMatch(const ArrayInterface<char>&& DelimAny,ArrayBridge<char>& Data,bool KeepDelim);
+	bool		Pop(const std::string Delim,std::string& Data,bool KeepDelim);
+	bool		Pop(const ArrayBridge<char>&& Delim,ArrayBridge<char>&& Data,bool KeepDelim);
+	
+	bool		Pop(size_t Length);
+	bool		Pop(size_t Length,ArrayBridge<char>& Data);
+	inline bool	Pop(size_t Length,ArrayBridge<char>&& Data)	{	return Pop(Length, Data);	}
+	bool		Pop(std::string RegexPattern,ArrayBridge<std::string>& Parts);	//	gr: using string pattern because you can't get the pattern from std::regex
+	
+	bool		Push(const std::string& Data);
+	bool		Push(const ArrayBridge<char>& Data);
+	bool		UnPop(const ArrayBridge<char>& Data);
+	inline bool	UnPop(const ArrayBridge<char>&& Data)	{	return UnPop( Data );	}
+	bool		UnPop(const std::string& Data);
+	
+	bool		IsEmpty() const				{	return GetBufferedSize() == 0;	}
+	size_t		GetBufferedSize() const		{	return mData.GetDataSize();	}
+	
+	bool		PeekBack(ArrayBridge<char>&& Data);	//	copy last X bytes without modifying. fails if this many bytes don't exist
+	
+public:
+	SoyEvent<bool>	mOnDataPushed;
+	
+private:
+	//	gr: make this a ring buffer for speed!
+	std::mutex		mLock;
+	Array<char>		mData;
 };
+
+
+
+class TStreamReader : public SoyWorkerThread
+{
+public:
+	TStreamReader(const std::string& Name);
+	
+	virtual bool							Iteration() override;
+	virtual void							Read()=0;			//	read next chunk of data into buffer
+	virtual std::shared_ptr<Soy::TReadProtocol>		AllocProtocol()=0;	//	alloc a new protocol instance to process incoming data
+	
+public:
+	SoyEvent<std::shared_ptr<Soy::TReadProtocol>>	mOnDataRecieved;
+	
+protected:
+	TStreamBuffer							mReadBuffer;
+	std::shared_ptr<Soy::TReadProtocol>		mCurrentProtocol;
+};
+
+
+
+
+class TStreamWriter : public SoyWorkerThread
+{
+public:
+	TStreamWriter(const std::string& Name);
+	
+	virtual bool							Iteration() override;
+	void									Push(std::shared_ptr<Soy::TWriteProtocol>& Data);
+
+protected:
+	virtual void							Write(TStreamBuffer& Buffer)=0;	//	write next chunk
+
+public:
+	SoyEvent<std::shared_ptr<Soy::TWriteProtocol>>	mOnDataRecieved;
+	
+protected:
+	std::shared_ptr<Soy::TWriteProtocol>	mCurrentProtocol;
+
+private:
+	std::mutex										mQueueLock;
+	Array<std::shared_ptr<Soy::TWriteProtocol>>		mQueue;
+};
+
 
 
