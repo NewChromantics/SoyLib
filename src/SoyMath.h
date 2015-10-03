4a1fea7744696d064efbeba6e55d905513179293
Graham Reeves <graham@grahamreeves.com>
2015-10-03 13:49:11 +0100

    Added SoyMath AngleDegDiff

--- src/SoyMath.h
+++ src/SoyMath.h
@@ -33,6 +33,8 @@ namespace Soy
 	}
 	
 	
+	float	AngleDegDiff(float Angle,float Base);	//	get the smallest signed difference from Base
+	
 	template<typename T>
 	T	Lerp(const T& Start,const T& End,float Time)
 	{
