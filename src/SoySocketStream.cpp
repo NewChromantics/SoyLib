ac841228939a1de86d2910f3f159909f5ed04827
Graham Reeves <graham@grahamreeves.com>
2015-10-27 22:19:28 +0000

    Added (moved from popmovie/cast) Socket & http stream read/write specialised threads

---
+++ src/SoySocketStream.cpp
@@ -0,0 +1,86 @@
+#include "SoySocketStream.h"
+
+
+
+
+TSocketReadThread::TSocketReadThread(std::shared_ptr<SoySocket>& Socket,SoyRef ConnectionRef) :
+TStreamReader	( "TSocketReadThread" ),
+mSocket			( Socket ),
+mConnectionRef	( ConnectionRef )
+{
+	
+}
+
+
+
+void TSocketReadThread::Read()
+{
+	SoySocketConnection	ClientSocket = mSocket->GetConnection( mConnectionRef );
+	if ( !ClientSocket.IsValid() )
+	{
+		//	lost connection
+		throw Soy::AssertException("Connection lost");
+	}
+	
+	//	gr use a larger static buffer so we can stream locally much faster than in 1024 chunks
+	//static int BufferSize = 1024*1024*20;	//	Xmb a time should be plenty... maybe query for the actual socket limit jsut so we don't go silly with memory
+	static int BufferSize = 100;	//	Xmb a time should be plenty... maybe query for the actual socket limit jsut so we don't go silly with memory
+	auto& Buffer = mRecvBuffer;
+	Buffer.SetSize( BufferSize );
+	
+	
+	//	throws on error
+	if ( !ClientSocket.Recieve( GetArrayBridge(Buffer) ) )
+	{
+		//	graceful close
+		throw Soy::AssertException("Gracefull close");
+	}
+	
+	mReadBuffer.Push( GetArrayBridge( Buffer ) );
+}
+
+
+
+TSocketWriteThread::TSocketWriteThread(std::shared_ptr<SoySocket>& Socket,SoyRef ConnectionRef) :
+TStreamWriter	( "TSocketWriteThread" ),
+mSocket			( Socket ),
+mConnectionRef	( ConnectionRef )
+{
+}
+
+void TSocketWriteThread::Write(TStreamBuffer& Buffer)
+{
+	//	keep writing until buffer is empty
+	while ( !Buffer.IsEmpty() )
+	{
+		SoySocketConnection	ClientSocket = mSocket->GetConnection( mConnectionRef );
+		if ( !ClientSocket.IsValid() )
+		{
+			//	lost connection
+			throw Soy::AssertException("Connection lost");
+		}
+		
+		static size_t PopMaxSize = 1024 * 1024 * 1;
+		size_t PopSize = std::min( PopMaxSize, Buffer.GetBufferedSize() );
+		if ( !Buffer.Pop( PopSize, GetArrayBridge(mSendBuffer) ) )
+		{
+			std::Debug << "Buffer data has gone missing. Trying again" << std::endl;
+			continue;
+		}
+		
+		//	write to socket
+		try
+		{
+			ClientSocket.Send( GetArrayBridge(mSendBuffer), mSocket->IsUdp() );
+		}
+		catch (std::exception& e)
+		{
+			mSocket->OnError( mConnectionRef, e.what() );
+			throw;
+		}
+	}
+}
+
+
+
+
