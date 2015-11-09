ae8fca17ce830e3418a6b02378f33d6517221f53
Graham Reeves <graham@grahamreeves.com>
2015-11-09 18:58:34 +0000

    Added SPS reading. (Move BitReader class!)

--- src/SoyH264.cpp
+++ src/SoyH264.cpp
@@ -458,7 +458,489 @@ void H264::DecodeNaluByte(uint8 Byte,H264NaluContent::Type& Content,H264NaluPrio
 }
 
 
+class TBitReader
+{
+public:
+	TBitReader(const ArrayBridge<uint8>& Data) :
+		mData	( Data ),
+		mBitPos	( 0 )
+	{
+	}
+	TBitReader(const ArrayBridge<uint8>&& Data) :
+		mData	( Data ),
+		mBitPos	( 0 )
+	{
+	}
+	
+	void			Read(uint32& Data,size_t BitCount);
+	void			Read(uint64& Data,size_t BitCount);
+	void			Read(uint8& Data,size_t BitCount);
+	size_t			BitPosition() const					{	return mBitPos;	}
+	
+	template<int BYTECOUNT,typename STORAGE>
+	void			ReadBytes(STORAGE& Data,size_t BitCount);
 
+	void			ReadExponentialGolombCode(uint32& Data);
+	void			ReadExponentialGolombCodeSigned(int32& Data);
+	
+private:
+	const ArrayBridge<uint8>&	mData;
+	size_t				mBitPos;	//	current bit-to-read/write-pos (the tail)
+};
+
+/*
+unsigned int ReadBit()
+{
+	ATLASSERT(m_nCurrentBit <= m_nLength * 8);
+	int nIndex = m_nCurrentBit / 8;
+	int nOffset = m_nCurrentBit % 8 + 1;
+	
+	m_nCurrentBit ++;
+	return (m_pStart[nIndex] >> (8-nOffset)) & 0x01;
+}
+*/
+
+void TBitReader::ReadExponentialGolombCode(uint32& Data)
+{
+	int i = 0;
+	
+	while( true )
+	{
+		uint8 Bit;
+		Read( Bit, 1 );
+		
+		if ( (Bit == 0) && (i < 32) )
+			i++;
+		else
+			break;
+	}
+	Read( Data, i );
+	Data += (1 << i) - 1;
+}
+
+void TBitReader::ReadExponentialGolombCodeSigned(int32& Data)
+{
+	uint32 r;
+	ReadExponentialGolombCode(r);
+	if (r & 0x01)
+	{
+		Data = (r+1)/2;
+	}
+	else
+	{
+		Data = -(r/2);
+	}
+}
+
+
+template<int BYTECOUNT,typename STORAGE>
+void TBitReader::ReadBytes(STORAGE& Data,size_t BitCount)
+{
+	//	gr: definitly correct
+	Data = 0;
+	
+	BufferArray<uint8,BYTECOUNT> Bytes;
+	auto ComponentBitCount = BitCount;
+	while ( ComponentBitCount > 0 )
+	{
+		Read( Bytes.PushBack(), std::min(8ul,ComponentBitCount) );
+		ComponentBitCount -= 8;
+	}
+	
+	Data = 0;
+	for ( int i=0;	i<Bytes.GetSize();	i++ )
+	{
+		int Shift = (i * 8);
+		Data |= static_cast<STORAGE>(Bytes[i]) << Shift;
+	}
+	
+	STORAGE DataBackwardTest = 0;
+	for ( int i=0;	i<Bytes.GetSize();	i++ )
+	{
+		auto Shift = (Bytes.GetSize()-1-i) * 8;
+		DataBackwardTest |= static_cast<STORAGE>(Bytes[i]) << Shift;
+	}
+	
+	//	turns out THIS is the right way
+	Data = DataBackwardTest;
+}
+
+void TBitReader::Read(uint32& Data,size_t BitCount)
+{
+	//	break up data
+	if ( BitCount <= 8 )
+	{
+		uint8 Data8;
+		Read( Data8, BitCount );
+		Data = Data8;
+		return;
+	}
+	
+	if ( BitCount <= 8 )
+	{
+		ReadBytes<1>( Data, BitCount );
+		return;
+	}
+	
+	if ( BitCount <= 16 )
+	{
+		ReadBytes<2>( Data, BitCount );
+		return;
+	}
+	
+	if ( BitCount <= 32 )
+	{
+		ReadBytes<4>( Data, BitCount );
+		return;
+	}
+	
+	std::stringstream Error;
+	Error << __func__ << " not handling bit count > 32; " << BitCount;
+	throw Soy::AssertException( Error.str() );
+}
+
+
+void TBitReader::Read(uint64& Data,size_t BitCount)
+{
+	if ( BitCount <= 8 )
+	{
+		ReadBytes<1>( Data, BitCount );
+		return;
+	}
+	
+	if ( BitCount <= 16 )
+	{
+		ReadBytes<2>( Data, BitCount );
+		return;
+	}
+	
+	if ( BitCount <= 32 )
+	{
+		ReadBytes<4>( Data, BitCount );
+		return;
+	}
+	
+	if ( BitCount <= 64 )
+	{
+		ReadBytes<8>( Data, BitCount );
+		return;
+	}
+	
+	std::stringstream Error;
+	Error << __func__ << " not handling bit count > 32; " << BitCount;
+	throw Soy::AssertException( Error.str() );
+}
+
+void TBitReader::Read(uint8& Data,size_t BitCount)
+{
+	if ( BitCount <= 0 )
+		return;
+	Soy::Assert( BitCount <= 8, "trying to read>8 bits to 8bit value");
+	
+	//	current byte
+	auto CurrentByte = mBitPos / 8;
+	auto CurrentBit = mBitPos % 8;
+	
+	//	out of range
+		Soy::Assert( CurrentByte < mData.GetSize(), "Reading byte out of range");
+	
+	//	move along
+	mBitPos += BitCount;
+	
+	//	get byte
+	Data = mData[CurrentByte];
+	
+	//	pick out certain bits
+	//	gr: reverse endianess to what I thought...
+	//Data >>= CurrentBit;
+	Data >>= 8-CurrentBit-BitCount;
+	Data &= (1<<BitCount)-1;
+}
+
+
+const unsigned char * m_pStart;
+unsigned short m_nLength;
+int m_nCurrentBit;
+
+unsigned int ReadBit()
+{
+	assert(m_nCurrentBit <= m_nLength * 8);
+	int nIndex = m_nCurrentBit / 8;
+	int nOffset = m_nCurrentBit % 8 + 1;
+	
+	m_nCurrentBit ++;
+	return (m_pStart[nIndex] >> (8-nOffset)) & 0x01;
+}
+
+unsigned int ReadBits(int n)
+{
+	int r = 0;
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		r |= ( ReadBit() << ( n - i - 1 ) );
+	}
+	return r;
+}
+
+unsigned int ReadExponentialGolombCode()
+{
+	int r = 0;
+	int i = 0;
+	
+	while( (ReadBit() == 0) && (i < 32) )
+	{
+		i++;
+	}
+	
+	r = ReadBits(i);
+	r += (1 << i) - 1;
+	return r;
+}
+
+unsigned int ReadSE()
+{
+	int r = ReadExponentialGolombCode();
+	if (r & 0x01)
+	{
+		r = (r+1)/2;
+	}
+	else
+	{
+		r = -(r/2);
+	}
+	return r;
+}
+
+void Parse(const unsigned char * pStart, unsigned short nLen)
+{
+	m_pStart = pStart;
+	m_nLength = nLen;
+	m_nCurrentBit = 0;
+	
+	int frame_crop_left_offset=0;
+	int frame_crop_right_offset=0;
+	int frame_crop_top_offset=0;
+	int frame_crop_bottom_offset=0;
+	
+	int profile_idc = ReadBits(8);
+	int constraint_set0_flag = ReadBit();
+	int constraint_set1_flag = ReadBit();
+	int constraint_set2_flag = ReadBit();
+	int constraint_set3_flag = ReadBit();
+	int constraint_set4_flag = ReadBit();
+	int constraint_set5_flag = ReadBit();
+	int reserved_zero_2bits  = ReadBits(2);
+	int level_idc = ReadBits(8);
+	int seq_parameter_set_id = ReadExponentialGolombCode();
+	
+	
+	if( profile_idc == 100 || profile_idc == 110 ||
+	   profile_idc == 122 || profile_idc == 244 ||
+	   profile_idc == 44 || profile_idc == 83 ||
+	   profile_idc == 86 || profile_idc == 118 )
+	{
+		int chroma_format_idc = ReadExponentialGolombCode();
+		
+		if( chroma_format_idc == 3 )
+		{
+			int residual_colour_transform_flag = ReadBit();
+		}
+		int bit_depth_luma_minus8 = ReadExponentialGolombCode();
+		int bit_depth_chroma_minus8 = ReadExponentialGolombCode();
+		int qpprime_y_zero_transform_bypass_flag = ReadBit();
+		int seq_scaling_matrix_present_flag = ReadBit();
+		
+		if (seq_scaling_matrix_present_flag)
+		{
+			int i=0;
+			for ( i = 0; i < 8; i++)
+			{
+				int seq_scaling_list_present_flag = ReadBit();
+				if (seq_scaling_list_present_flag)
+				{
+					int sizeOfScalingList = (i < 6) ? 16 : 64;
+					int lastScale = 8;
+					int nextScale = 8;
+					int j=0;
+					for ( j = 0; j < sizeOfScalingList; j++)
+					{
+						if (nextScale != 0)
+						{
+							int delta_scale = ReadSE();
+							nextScale = (lastScale + delta_scale + 256) % 256;
+						}
+						lastScale = (nextScale == 0) ? lastScale : nextScale;
+					}
+				}
+			}
+		}
+	}
+	
+	int log2_max_frame_num_minus4 = ReadExponentialGolombCode();
+	int pic_order_cnt_type = ReadExponentialGolombCode();
+	if( pic_order_cnt_type == 0 )
+	{
+		int log2_max_pic_order_cnt_lsb_minus4 = ReadExponentialGolombCode();
+	}
+	else if( pic_order_cnt_type == 1 )
+	{
+		int delta_pic_order_always_zero_flag = ReadBit();
+		int offset_for_non_ref_pic = ReadSE();
+		int offset_for_top_to_bottom_field = ReadSE();
+		int num_ref_frames_in_pic_order_cnt_cycle = ReadExponentialGolombCode();
+		int i;
+		for( i = 0; i < num_ref_frames_in_pic_order_cnt_cycle; i++ )
+		{
+			ReadSE();
+			//sps->offset_for_ref_frame[ i ] = ReadSE();
+		}
+	}
+	int max_num_ref_frames = ReadExponentialGolombCode();
+	int gaps_in_frame_num_value_allowed_flag = ReadBit();
+	int pic_width_in_mbs_minus1 = ReadExponentialGolombCode();
+	int pic_height_in_map_units_minus1 = ReadExponentialGolombCode();
+	int frame_mbs_only_flag = ReadBit();
+	if( !frame_mbs_only_flag )
+	{
+		int mb_adaptive_frame_field_flag = ReadBit();
+	}
+	int direct_8x8_inference_flag = ReadBit();
+	int frame_cropping_flag = ReadBit();
+	if( frame_cropping_flag )
+	{
+		frame_crop_left_offset = ReadExponentialGolombCode();
+		frame_crop_right_offset = ReadExponentialGolombCode();
+		frame_crop_top_offset = ReadExponentialGolombCode();
+		frame_crop_bottom_offset = ReadExponentialGolombCode();
+	}
+	int vui_parameters_present_flag = ReadBit();
+	pStart++;
+	
+	int Width = ((pic_width_in_mbs_minus1 +1)*16) - frame_crop_bottom_offset*2 - frame_crop_top_offset*2;
+	int Height = ((2 - frame_mbs_only_flag)* (pic_height_in_map_units_minus1 +1) * 16) - (frame_crop_right_offset * 2) - (frame_crop_left_offset * 2);
+	
+	printf("\n\nWxH = %dx%d\n\n",Width,Height);
+	
+}
+
+
+
+H264::TSpsParams H264::ParseSps(const ArrayBridge<uint8>&& Data)
+{
+	Parse( Data.GetArray(), Data.GetDataSize() );
+	
+	TSpsParams Params;
+	
+	TBitReader Reader( Data );
+//	http://stackoverflow.com/questions/12018535/get-the-width-height-of-the-video-from-h-264-nalu
+	Reader.Read( Params.mProfile, 8 );
+	Reader.Read( Params.mConstraintFlag[0], 1 );
+	Reader.Read( Params.mConstraintFlag[1], 1 );
+	Reader.Read( Params.mConstraintFlag[2], 1 );
+	Reader.Read( Params.mConstraintFlag[3], 1 );
+	Reader.Read( Params.mConstraintFlag[4], 1 );
+	Reader.Read( Params.mConstraintFlag[5], 1 );
+	
+	Reader.Read( Params.mReservedZero, 2 );
+	Reader.Read( Params.mLevel, 8 );
+	Reader.ReadExponentialGolombCode( Params.seq_parameter_set_id );
+	
+	
+	if( Params.mProfile == 100 || Params.mProfile == 110 ||
+	   Params.mProfile == 122 || Params.mProfile == 244 ||
+	   Params.mProfile == 44 || Params.mProfile == 83 ||
+	   Params.mProfile == 86 || Params.mProfile == 118 )
+	{
+		Reader.ReadExponentialGolombCode( Params.chroma_format_idc );
+		
+		if( Params.chroma_format_idc == 3 )
+		{
+			Reader.Read( Params.residual_colour_transform_flag, 1 );
+		}
+		
+		Reader.ReadExponentialGolombCode(Params.bit_depth_luma_minus8 );
+		Reader.ReadExponentialGolombCode(Params.bit_depth_chroma_minus8 );
+		Reader.ReadExponentialGolombCode(Params.qpprime_y_zero_transform_bypass_flag );
+		Reader.ReadExponentialGolombCode(Params.seq_scaling_matrix_present_flag );
+		
+		if (Params.seq_scaling_matrix_present_flag)
+		{
+			int i=0;
+			for ( i = 0; i < 8; i++)
+			{
+				Reader.Read(Params.seq_scaling_list_present_flag,1);
+				if (Params.seq_scaling_list_present_flag)
+				{
+					int sizeOfScalingList = (i < 6) ? 16 : 64;
+					int lastScale = 8;
+					int nextScale = 8;
+					int j=0;
+					for ( j = 0; j < sizeOfScalingList; j++)
+					{
+						if (nextScale != 0)
+						{
+							Soy_AssertTodo();
+							//int delta_scale = ReadSE();
+							//nextScale = (lastScale + delta_scale + 256) % 256;
+						}
+						lastScale = (nextScale == 0) ? lastScale : nextScale;
+					}
+				}
+			}
+		}
+	}
+	
+	
+	
+	Reader.ReadExponentialGolombCode( Params.log2_max_frame_num_minus4 );
+	Reader.ReadExponentialGolombCode( Params.pic_order_cnt_type );
+	if ( Params.pic_order_cnt_type == 0 )
+	{
+		Reader.ReadExponentialGolombCode( Params.log2_max_pic_order_cnt_lsb_minus4 );
+	}
+	else if ( Params.pic_order_cnt_type == 1 )
+	{
+		Reader.Read( Params.delta_pic_order_always_zero_flag, 1 );
+		
+		Reader.ReadExponentialGolombCodeSigned( Params.offset_for_non_ref_pic );
+		Reader.ReadExponentialGolombCodeSigned( Params.offset_for_top_to_bottom_field );
+		Reader.ReadExponentialGolombCode( Params.num_ref_frames_in_pic_order_cnt_cycle );
+		for( int i = 0; i <Params.num_ref_frames_in_pic_order_cnt_cycle; i++ )
+		{
+			int32 Dummy;
+			Reader.ReadExponentialGolombCodeSigned( Dummy );
+			//sps->offset_for_ref_frame[ i ] = ReadSE();
+		}
+	}
+	
+	Reader.ReadExponentialGolombCode( Params.num_ref_frames );
+	Reader.Read( Params.gaps_in_frame_num_value_allowed_flag, 1 );
+	Reader.ReadExponentialGolombCode( Params.pic_width_in_mbs_minus_1 );
+	Reader.ReadExponentialGolombCode( Params.pic_height_in_map_units_minus_1 );
+	Reader.Read( Params.frame_mbs_only_flag, 1 );
+	if( !Params.frame_mbs_only_flag )
+	{
+		Reader.Read( Params.mb_adaptive_frame_field_flag, 1 );
+	}
+	Reader.Read( Params.direct_8x8_inference_flag, 1 );
+	Reader.Read( Params.frame_cropping_flag, 1 );
+	if( Params.frame_cropping_flag )
+	{
+		Reader.ReadExponentialGolombCode( Params.frame_crop_left_offset );
+		Reader.ReadExponentialGolombCode( Params.frame_crop_right_offset );
+		Reader.ReadExponentialGolombCode( Params.frame_crop_top_offset );
+		Reader.ReadExponentialGolombCode( Params.frame_crop_bottom_offset );
+	}
+	Reader.Read( Params.vui_prameters_present_flag, 1 );
+	Reader.Read( Params.rbsp_stop_one_bit, 1 );
+	
+	Params.mWidth = ((Params.pic_width_in_mbs_minus_1 +1)*16) - Params.frame_crop_bottom_offset*2 - Params.frame_crop_top_offset*2;
+	Params.mHeight = ((2 - Params.frame_mbs_only_flag)* (Params.pic_height_in_map_units_minus_1 +1) * 16) - (Params.frame_crop_right_offset * 2) - (Params.frame_crop_left_offset * 2);
+
+	
+	return Params;
+}
 
 
 
--- src/SoyH264.h
+++ src/SoyH264.h
@@ -63,6 +63,8 @@ namespace H264NaluPriority
 
 namespace H264
 {
+	class TSpsParams;
+	
 	bool		ResolveH264Format(SoyMediaFormat::Type& Format,ArrayBridge<uint8>& Data);
 	inline bool	ResolveH264Format(SoyMediaFormat::Type& Format,ArrayBridge<uint8>&& Data)	{	return ResolveH264Format( Format, Data );	}
 	void		ConvertToEs(SoyMediaFormat::Type& Format,ArrayBridge<uint8>&& Data);
@@ -78,5 +80,53 @@ namespace H264
 	
 	uint8		EncodeNaluByte(H264NaluContent::Type Content,H264NaluPriority::Type Priority);
 	void		DecodeNaluByte(uint8 Byte,H264NaluContent::Type& Content,H264NaluPriority::Type& Priority);	//	throws on error (eg. reservered-zero not zero)
+	
+	TSpsParams	ParseSps(const ArrayBridge<uint8>&& Data);
 }
+
+
+class H264::TSpsParams
+{
+public:
+	TSpsParams()
+	{
+		memset( this, 0, sizeof(*this) );
+	}
+	size_t		mWidth;
+	size_t		mHeight;
+	uint8		mProfile;
+	uint8		mConstraintFlag[6];
+	uint8		mReservedZero;
+	uint8		mLevel;
+	uint32		seq_parameter_set_id;
+	uint32		chroma_format_idc;
+	uint8		residual_colour_transform_flag;
+	uint32		bit_depth_luma_minus8;
+	uint32		bit_depth_chroma_minus8;
+	uint32		qpprime_y_zero_transform_bypass_flag;
+	uint32		seq_scaling_matrix_present_flag;
+	uint32		seq_scaling_list_present_flag;
+	uint32		log2_max_frame_num_minus4;
+	uint32		pic_order_cnt_type;
+	uint32		log2_max_pic_order_cnt_lsb_minus4;
+	uint32		num_ref_frames;
+	uint32		gaps_in_frame_num_value_allowed_flag;
+	uint32		pic_width_in_mbs_minus_1;
+	uint32		pic_height_in_map_units_minus_1;
+	uint32		frame_mbs_only_flag;
+	uint32		direct_8x8_inference_flag;
+	uint32		frame_cropping_flag;
+	uint32		vui_prameters_present_flag;
+	uint32		rbsp_stop_one_bit;
+	uint8		mb_adaptive_frame_field_flag;
+	uint8		delta_pic_order_always_zero_flag;
+	uint32		frame_crop_left_offset;
+	uint32		frame_crop_right_offset;
+	uint32		frame_crop_top_offset;
+	uint32		frame_crop_bottom_offset;
+	
+	int32		offset_for_non_ref_pic;
+	int32		offset_for_top_to_bottom_field;
+	uint32		num_ref_frames_in_pic_order_cnt_cycle;
+};
 
