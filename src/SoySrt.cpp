984ffa237618f5b675467cbc2a97c7a4e2c7fd2e
Graham Reeves <graham@grahamreeves.com>
2016-01-18 19:59:39 +0000

    Android can't run our subtitle time code regex, so made a string-popping verison

--- src/SoySrt.cpp
+++ src/SoySrt.cpp
@@ -11,6 +11,8 @@ namespace Srt
 	void		EncodeTime(std::stringstream& Out,const SoyTime& Time);
 	void		DecodeTimeRange(const std::string& TimeString,SoyTime& Start,SoyTime& End);
 	void		EncodeTimeRange(std::stringstream& Out,const SoyTime& Start,const SoyTime& End);
+
+	void		DecodeTimeRange_NoRegex(const std::string& OrigTimeString,SoyTime& Start,SoyTime& End);
 	
 	//	gr: something(osx textedit?) stripped my linefeeds, so we might as well support them anyway
 	const char* LineFeed_rn = "\r\n";
@@ -76,18 +78,95 @@ void Srt::EncodeTime(std::stringstream& Out,const SoyTime& Time)
 	Out << Ms;
 }
 
+const char* RegexErrorToString(std::regex_error& Error)
+{
+	switch ( Error.code() )
+	{
+		case std::regex_constants::error_collate:		return "error_collate";
+		case std::regex_constants::error_ctype:			return "error_ctype";
+		case std::regex_constants::error_escape:		return "error_escape";
+		case std::regex_constants::error_backref:		return "error_backref";
+		case std::regex_constants::error_brack:			return "error_brack/mis matched []";
+		case std::regex_constants::error_paren:			return "error_paren/mis matched ()";
+		case std::regex_constants::error_brace:			return "error_brace";
+		case std::regex_constants::error_badbrace:		return "error_badbrace";
+		case std::regex_constants::error_range:			return "error_range";
+		case std::regex_constants::error_space:			return "error_space";
+		case std::regex_constants::error_badrepeat:		return "error_badrepeat";
+		case std::regex_constants::error_complexity:	return "error_complexity";
+		case std::regex_constants::error_stack:			return "error_stack";
+#if defined(TARGET_OSX)
+		case std::regex_constants::__re_err_grammar:	return "__re_err_grammar";
+		case std::regex_constants::__re_err_empty:		return "__re_err_empty";
+		case std::regex_constants::__re_err_unknown:	return "__re_err_unknown";
+#endif
+		default:
+			return "Unknown regex error code";
+	};
+}
+
+void Srt::DecodeTimeRange_NoRegex(const std::string& OrigTimeString,SoyTime& Start,SoyTime& End)
+{
+	auto TimeString = OrigTimeString;
+	
+	auto Match1 = Soy::StringPopUntil( TimeString, ':' );
+	auto Match2 = Soy::StringPopUntil( TimeString, ':' );
+	auto Match3 = Soy::StringPopUntil( TimeString, ',' );
+	
+	//	eat these
+	auto IsArrowChar = [](char Char)
+	{
+		return Char == ' ' || Char == '-' || Char == '>';
+	};
+	auto Match4 = Soy::StringPopUntil( TimeString, IsArrowChar );
+	Soy::StringTrimLeft( TimeString, IsArrowChar );
+	auto Match5 = Soy::StringPopUntil( TimeString, ':' );
+	auto Match6 = Soy::StringPopUntil( TimeString, ':' );
+	auto Match7 = Soy::StringPopUntil( TimeString, ',' );
+	auto Match8 = TimeString;
+	
+	/*
+	std::Debug << "SRT split (" << OrigTimeString << ") found ["
+					<< Match1 << "|"
+					<< Match2 << "|"
+					<< Match3 << "|" << Match4 << "|" << Match5 << "|" << Match6 << "|" << Match7 << "|" << Match8 << "]" << std::endl;
+	*/
+	Start = DecodeTime( Match1, Match2, Match3, Match4 );
+	End = DecodeTime( Match5, Match6, Match7, Match8 );
+}
+
 void Srt::DecodeTimeRange(const std::string& TimeString,SoyTime& Start,SoyTime& End)
 {
-	static const char* PatternStr = "^([0-9]+):([0-9]+):([0-9]+),([0-9]+) --> ([0-9]+):([0-9]+):([0-9]+),([0-9]+)$";
-	std::regex Pattern(PatternStr);
+	//	gr: android either won't support brackets, or regex_match fails
+	DecodeTimeRange_NoRegex( TimeString, Start, End );
+	return;
+
+	//	http://stackoverflow.com/a/8060116/355753
+	std::regex_constants::syntax_option_type Flags = std::regex_constants::basic;
+	//static const char* PatternStr = "^([0-9]+):([0-9]+):([0-9]+),([0-9]+) --> ([0-9]+):([0-9]+):([0-9]+),([0-9]+)$";
+	static const char* PatternStr = "([0-9]+)";
+	std::Debug << "making pattern... #" << PatternStr << "#" << std::endl;
+	
 	std::smatch Match;
-	if ( !std::regex_match( TimeString, Match, Pattern ) )
+	try
+	{
+		std::regex Pattern(PatternStr, Flags);
+		std::Debug << "std::regex_match(" << TimeString << ")..." << std::endl;
+		if ( !std::regex_match( TimeString, Match, Pattern ) )
+		{
+			std::stringstream Error;
+			Error << "Failed to parse time range; \"" << TimeString << "\"";
+			throw Soy::AssertException( Error.str() );
+		}
+		std::Debug << "regex matched OKAY (" <<  PatternStr << ")" << std::endl;
+	}
+	catch(std::regex_error& e)
 	{
 		std::stringstream Error;
-		Error << "Failed to parse time range; \"" << TimeString << "\"";
+		Error << "regex error: " << RegexErrorToString(e);
 		throw Soy::AssertException( Error.str() );
 	}
-
+	
 	Start = DecodeTime( Match[1].str(), Match[2].str(), Match[3].str(), Match[4].str() );
 	End = DecodeTime( Match[5].str(), Match[6].str(), Match[7].str(), Match[8].str() );
 }
