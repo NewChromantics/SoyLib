0633859e4884c8560187b3ea82e58ab0c4792003
Graham Reeves <graham@grahamreeves.com>
2016-02-25 13:47:52 +0000

    Added a deprecated and noexcept prefix so windows can use them

--- src/SoyTypes_Windows.h
+++ src/SoyTypes_Windows.h
@@ -46,6 +46,8 @@
 //	gr: delspec's need to go BEFORE function declarations on windows... find a nice workaround that isn't __deprecated(int myfunc());
 #define __noexcept		//	__declspec(nothrow)
 #define __deprecated	//	__declspec(deprecated)
+#define __noexcept_prefix	__declspec(nothrow)
+#define __deprecated_prefix	__declspec(deprecated)
 
 
 #include <math.h>
