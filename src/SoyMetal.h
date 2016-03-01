47ccc2dda85eac2cd864e2d5d3d75bf19f20fcee
Graham Reeves <graham@grahamreeves.com>
2016-03-01 15:26:37 +0000

    Expanded metal stubs a bit more. Handling OSX half-support

--- src/SoyMetal.h
+++ src/SoyMetal.h
@@ -11,15 +11,42 @@
 
 namespace Metal
 {
+	class TDeviceMeta;
 	class TDevice;
 	class TContext;
 	class TContextThread;		//	job queue
+	class TTexture;
 
 	void		EnumDevices(ArrayBridge<std::shared_ptr<TDevice>>&& Devices);
 };
 
 
+//	forward declarations matching unity's helpers
+#if defined(__OBJC__)
+@class CAMetalLayer;
+@protocol CAMetalDrawable;
+@protocol MTLDrawable;
+@protocol MTLDevice;
+@protocol MTLTexture;
+@protocol MTLCommandBuffer;
+@protocol MTLCommandQueue;
+@protocol MTLCommandEncoder;
 
+typedef id<CAMetalDrawable>		CAMetalDrawableRef;
+typedef id<MTLDevice>			MTLDeviceRef;
+typedef id<MTLTexture>			MTLTextureRef;
+typedef id<MTLCommandBuffer>	MTLCommandBufferRef;
+typedef id<MTLCommandQueue>		MTLCommandQueueRef;
+typedef id<MTLCommandEncoder>	MTLCommandEncoderRef;
+#else
+typedef struct objc_object		CAMetalLayer;
+typedef struct objc_object*		CAMetalDrawableRef;
+typedef struct objc_object*		MTLDeviceRef;
+typedef struct objc_object*		MTLTextureRef;
+typedef struct objc_object*		MTLCommandBufferRef;
+typedef struct objc_object*		MTLCommandQueueRef;
+typedef struct objc_object*		MTLCommandEncoderRef;
+#endif
 
 
 class Metal::TDeviceMeta
@@ -38,40 +65,39 @@ std::ostream& operator<<(std::ostream &out,const Metal::TDeviceMeta& in);
 class Metal::TDevice : public TDeviceMeta
 {
 public:
-	TDevice();
-	~TDevice();
-	
+	TDevice(void* DevicePtr);
+
 	std::shared_ptr<TContext>		CreateContext();
 	std::shared_ptr<TContextThread>	CreateContextThread(const std::string& Name);
 	
 	TDeviceMeta			GetMeta() const	{	return *this;	}
 	
+	bool				operator==(void* DevicePtr) const		{	return mDevice == DevicePtr;	}
+	
 protected:
-	id<MTLDevice>		mDevice;
+	MTLDeviceRef		mDevice;
 };
 
 
 class Metal::TContext : public PopWorker::TJobQueue, public PopWorker::TContext
 {
 public:
-	TContext(TDevice& Device);
-	~TContext();
+	TContext(void* DevicePtr);	//	unity's anonymous interface
+	//TContext(TDevice& Device);
 	
 	virtual bool	Lock() override;
 	virtual void	Unlock() override;
 	virtual bool	IsLocked(std::thread::id Thread) override;
+	void			Iteration()			{	Flush(*this);	}
 	
 protected:
-	TDevice&			mDevice;
+	std::shared_ptr<TDevice>	mDevice;
 	
 private:
 	std::thread::id		mLockedThread;	//	needed in the context as it gets locked in other places than the job queue
 };
 
-#if defined(__OBJC__)
-template<typename TYPE>
-class ObjcPtr
-
+/*
 class Metal::TContextThread : public SoyWorkerThread, public Metal::TContext
 {
 public:
@@ -86,6 +112,19 @@ public:
 	//	thread
 	virtual bool		Iteration() override	{	PopWorker::TJobQueue::Flush(*this);	return true;	}
 	virtual bool		CanSleep() override		{	return !PopWorker::TJobQueue::HasJobs();	}	//	break out of conditional with this
+};
+*/
+
+class Metal::TTexture
+{
+public:
+	//	referece from external (unity)
+	TTexture(void* TexturePtr);
+	
+	SoyPixelsMeta	GetMeta() const;
+	
+private:
+	MTLTextureRef	mTexture;
 };
 
 
--- src/SoyMetal.mm
+++ src/SoyMetal.mm
@@ -1,13 +1,148 @@
 #include "SoyMetal.h"
 #include <SoyDebug.h>
 #include <SoyString.h>
-#include <Metal/Metal.h>
+
+#if defined(TARGET_IOS)
+#define ENABLE_METAL
+#elif defined(TARGET_OSX) && defined(AVAILABLE_MAC_OS_X_VERSION_10_11_AND_LATER)
+//	need 10.11 sdk for metal on osx
+#define ENABLE_METAL
+#endif
+
+#if defined(ENABLE_METAL)
+#import <Metal/Metal.h>
+#else
+
+typedef NSUInteger MTLPixelFormat;
+enum
+{
+	MTLPixelFormatBGRA8Unorm,
+	MTLPixelFormatBGRA8Unorm_sRGB,
+};
+
+@protocol MTLDevice
+- (id <MTLCommandQueue>)newCommandQueue;
+@end
+
+@protocol MTLCommandBuffer
+- (void)presentDrawable:(id <MTLDrawable>)drawable;
+@end
+
+#endif
 
 namespace Metal
 {
 	std::mutex						DevicesLock;
 	Array<std::shared_ptr<TDevice>>	Devices;
+	
+
+	SoyPixelsFormat::Type			GetPixelFormat(MTLPixelFormat Format);
+	MTLPixelFormat					GetPixelFormat(SoyPixelsFormat::Type Format);
+	
+	std::shared_ptr<TDevice>		GetAnonymousDevice(void* DevicePtr);
 }
+
+
+
+
+//	todo: abstract MediaFoundation templated version of this
+
+#define CV_VIDEO_TYPE_META(Enum,SoyFormat)	TCvVideoTypeMeta<MTLPixelFormat>( Enum, #Enum, SoyFormat )
+#define CV_VIDEO_INVALID_ENUM		MTLPixelFormatInvalid
+template<typename PLATFORMFORMAT>
+class TCvVideoTypeMeta
+{
+public:
+	TCvVideoTypeMeta(PLATFORMFORMAT Enum,const char* EnumName,SoyPixelsFormat::Type SoyFormat) :
+		mPlatformFormat		( Enum ),
+		mName				( EnumName ),
+		mSoyFormat			( SoyFormat )
+	{
+		Soy::Assert( IsValid(), "Expected valid enum - or invalid enum is bad" );
+	}
+	TCvVideoTypeMeta() :
+		mPlatformFormat		( CV_VIDEO_INVALID_ENUM ),
+		mName				( "Invalid enum" ),
+		mSoyFormat			( SoyPixelsFormat::Invalid )
+	{
+	}
+	
+	bool		IsValid() const		{	return mPlatformFormat != CV_VIDEO_INVALID_ENUM;	}
+	
+	bool		operator==(const PLATFORMFORMAT& Enum) const			{	return mPlatformFormat == Enum;	}
+	bool		operator==(const SoyPixelsFormat::Type& Format) const	{	return mSoyFormat == Format;	}
+	
+public:
+	PLATFORMFORMAT			mPlatformFormat;
+	SoyPixelsFormat::Type	mSoyFormat;
+	std::string				mName;
+};
+
+
+
+
+TCvVideoTypeMeta<MTLPixelFormat> PixelFormatMap[] =
+{
+	CV_VIDEO_TYPE_META( MTLPixelFormatA8Unorm,		SoyPixelsFormat::Greyscale ),
+	CV_VIDEO_TYPE_META( MTLPixelFormatR8Unorm,		SoyPixelsFormat::Greyscale ),
+	CV_VIDEO_TYPE_META( MTLPixelFormatR8Unorm_sRGB,	SoyPixelsFormat::Greyscale ),
+	CV_VIDEO_TYPE_META( MTLPixelFormatR8Snorm,		SoyPixelsFormat::Greyscale ),
+	CV_VIDEO_TYPE_META( MTLPixelFormatR8Uint,		SoyPixelsFormat::Greyscale ),
+	CV_VIDEO_TYPE_META( MTLPixelFormatR8Sint,		SoyPixelsFormat::Greyscale ),
+	CV_VIDEO_TYPE_META( MTLPixelFormatStencil8,		SoyPixelsFormat::Greyscale ),
+	
+	CV_VIDEO_TYPE_META( MTLPixelFormatRG8Unorm,		SoyPixelsFormat::GreyscaleAlpha ),
+	CV_VIDEO_TYPE_META( MTLPixelFormatRG8Unorm_sRGB,	SoyPixelsFormat::GreyscaleAlpha ),
+	CV_VIDEO_TYPE_META( MTLPixelFormatRG8Uint,		SoyPixelsFormat::GreyscaleAlpha ),
+	CV_VIDEO_TYPE_META( MTLPixelFormatRG8Sint,		SoyPixelsFormat::GreyscaleAlpha ),
+	
+	CV_VIDEO_TYPE_META( MTLPixelFormatRGBA8Unorm,		SoyPixelsFormat::RGBA ),
+	CV_VIDEO_TYPE_META( MTLPixelFormatRGBA8Unorm_sRGB,	SoyPixelsFormat::RGBA ),
+	CV_VIDEO_TYPE_META( MTLPixelFormatRGBA8Snorm,		SoyPixelsFormat::RGBA ),
+	CV_VIDEO_TYPE_META( MTLPixelFormatRGBA8Uint,		SoyPixelsFormat::RGBA ),
+	CV_VIDEO_TYPE_META( MTLPixelFormatRGBA8Sint,		SoyPixelsFormat::RGBA ),
+	
+	CV_VIDEO_TYPE_META( MTLPixelFormatBGRA8Unorm,		SoyPixelsFormat::BGRA ),
+	CV_VIDEO_TYPE_META( MTLPixelFormatBGRA8Unorm_sRGB,	SoyPixelsFormat::BGRA ),
+
+	/*
+			 @constant MTLPixelFormatGBGR422
+			 @abstract A pixel format where the red and green channels are subsampled horizontally.  Two pixels are stored in 32 bits, with shared red and blue values, and unique green values.
+			 @discussion This format is equivelent to YUY2, YUYV, yuvs, or GL_RGB_422_APPLE/GL_UNSIGNED_SHORT_8_8_REV_APPLE.   The component order, from lowest addressed byte to highest, is Y0, Cb, Y1, Cr.  There is no implicit colorspace conversion from YUV to RGB, the shader will receive (Cr, Y, Cb, 1).  422 textures must have a width that is a multiple of 2, and can only be used for 2D non-mipmap textures.  When sampling, ClampToEdge is the only usable wrap mode.
+			MTLPixelFormatGBGR422 = 240,
+			
+			 @constant MTLPixelFormatBGRG422
+			 @abstract A pixel format where the red and green channels are subsampled horizontally.  Two pixels are stored in 32 bits, with shared red and blue values, and unique green values.
+			 @discussion This format is equivelent to UYVY, 2vuy, or GL_RGB_422_APPLE/GL_UNSIGNED_SHORT_8_8_APPLE. The component order, from lowest addressed byte to highest, is Cb, Y0, Cr, Y1.  There is no implicit colorspace conversion from YUV to RGB, the shader will receive (Cr, Y, Cb, 1).  422 textures must have a width that is a multiple of 2, and can only be used for 2D non-mipmap textures.  When sampling, ClampToEdge is the only usable wrap mode.
+			MTLPixelFormatBGRG422 = 241,
+*/
+};
+
+
+MTLPixelFormat Metal::GetPixelFormat(SoyPixelsFormat::Type Format)
+{
+	auto Table = GetRemoteArray( PixelFormatMap );
+	auto* Meta = GetArrayBridge(Table).Find( Format );
+	
+	if ( !Meta )
+		return CV_VIDEO_INVALID_ENUM;
+	
+	return Meta->mPlatformFormat;
+}
+
+SoyPixelsFormat::Type Metal::GetPixelFormat(MTLPixelFormat Format)
+{
+	auto Table = GetRemoteArray( PixelFormatMap );
+	auto* Meta = GetArrayBridge(Table).Find( Format );
+	
+	if ( !Meta )
+		return SoyPixelsFormat::Invalid;
+	
+	return Meta->mSoyFormat;
+}
+
+
+
 
 
 
@@ -16,17 +151,101 @@ void Metal::EnumDevices(ArrayBridge<std::shared_ptr<TDevice>>&& Devices)
 	//	refresh list
 	std::lock_guard<std::mutex> Lock( DevicesLock );
 	
+#if defined(TARGET_OSX)
 	NSArray<id<MTLDevice>>* MtlDevices = MTLCopyAllDevices();
+	NSArray<id<MTLDevice>>* MtlDevices = MTLCopyAllDevices();
+#else
+	NSMutableArray<id<MTLDevice>>* MtlDevices = [[NSMutableArray alloc] init];
+
+	auto DefaultDevice = MTLCreateSystemDefaultDevice();
+	if ( DefaultDevice )
+		[MtlDevices addObject:DefaultDevice];
+#endif
+
 	for ( int d=0;	MtlDevices && d<[MtlDevices count];	d++ )
 	{
-		MTLDevice* MtlDevice = [MtlDevices objectAtIndex:d];
+		id<MTLDevice> MtlDevice = [MtlDevices objectAtIndex:d];
 		if ( !MtlDevice )
 		{
 			std::Debug << "Warning: Metal device " << d << "/" << ([MtlDevices count]) << " null " << std::endl;
 			continue;
 		}
+	}
 
-		
+}
+
+std::shared_ptr<Metal::TDevice> Metal::GetAnonymousDevice(void* DevicePtr)
+{
+	static std::shared_ptr<TDevice> AnonDevice;
+	
+	if ( AnonDevice && !(*AnonDevice == DevicePtr) )
+	{
+		throw Soy::AssertException("New anonymous metal device. Handle this");
+	}
+	
+	if ( !AnonDevice )
+	{
+		AnonDevice.reset( new TDevice(DevicePtr) );
+	}
+	
+	return AnonDevice;
 }
 
 
+Metal::TTexture::TTexture(void* TexturePtr) :
+	mTexture	( nullptr )
+{
+	mTexture = (__bridge MTLTextureRef)TexturePtr;
+	Soy::Assert( mTexture != nil, "Expected texture?");
+}
+	
+SoyPixelsMeta Metal::TTexture::GetMeta() const
+{
+	if ( !mTexture )
+		return SoyPixelsMeta();
+
+	auto Format = Metal::GetPixelFormat( [mTexture pixelFormat] );
+	auto Width = [mTexture width];
+	auto Height = [mTexture height];
+	return SoyPixelsMeta( Width, Height, Format );
+}
+
+
+Metal::TDevice::TDevice(void* DevicePtr)
+{
+	
+}
+
+
+Metal::TContext::TContext(void* DevicePtr)
+{
+	mDevice = GetAnonymousDevice( DevicePtr );
+	Soy::Assert( mDevice != nullptr, "Couldnt get anonymous metal device");
+	
+}
+
+
+bool Metal::TContext::IsLocked(std::thread::id Thread)
+{
+	//	check for any thread
+	if ( Thread == std::thread::id() )
+		return mLockedThread!=std::thread::id();
+	else
+		return mLockedThread == Thread;
+}
+
+bool Metal::TContext::Lock()
+{
+	Soy::Assert( mLockedThread == std::thread::id(), "context already locked" );
+	
+	mLockedThread = std::this_thread::get_id();
+	return true;
+}
+
+void Metal::TContext::Unlock()
+{
+	auto ThisThread = std::this_thread::get_id();
+	Soy::Assert( mLockedThread != std::thread::id(), "context not locked to wrong thread" );
+	Soy::Assert( mLockedThread == ThisThread, "context not unlocked from wrong thread" );
+	mLockedThread = std::thread::id();
+}
