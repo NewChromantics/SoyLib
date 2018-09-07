a0fa3765b999df4ef94b81f8b5a04d279bd5441e
Graham Reeves <graham@grahamreeves.com>
2018-09-07 15:29:51 +0100

    Added OnTryDragDrop callback for windows

--- src/SoyWindow.h
+++ src/SoyWindow.h
@@ -2,6 +2,7 @@
 
 #include <SoyEvent.h>
 #include <SoyMath.h>
+#include <array.hpp>
 
 //	gr: this might want expanding later for multiple screens, mouse button number etc
 typedef vec2f TMousePos;
@@ -31,8 +32,10 @@ namespace Soy
 class SoyWindow
 {
 public:
-	std::function<void(const TMousePos&)>		mOnMouseDown;
-	std::function<void(const TMousePos&)>		mOnMouseMove;
-	std::function<void(const TMousePos&)>		mOnMouseUp;
+	std::function<void(const TMousePos&)>			mOnMouseDown;
+	std::function<void(const TMousePos&)>			mOnMouseMove;
+	std::function<void(const TMousePos&)>			mOnMouseUp;
+	std::function<bool(ArrayBridge<std::string>&)>	mOnTryDragDrop;
+	
 };
 
