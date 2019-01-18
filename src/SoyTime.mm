0dfdba851bee1a89720513d44d41c8076bb3b7b8
Graham Reeves <graham@grahamreeves.com>
2019-01-18 15:33:36 +0000

    Note for CoreMedia framework

--- src/SoyTime.mm
+++ src/SoyTime.mm
@@ -6,6 +6,7 @@ SoyTime Soy::Platform::GetTime(CMTime Time)
 	if ( CMTIME_IS_INVALID( Time ) )
 		return SoyTime();
 	
+	//	missing CMTimeGetSeconds ? link to the CoreMedia framework :)
 	Float64 TimeSec = CMTimeGetSeconds(Time);
 	UInt64 TimeMs = 1000.f*TimeSec;
 	return SoyTime( std::chrono::milliseconds(TimeMs) );
