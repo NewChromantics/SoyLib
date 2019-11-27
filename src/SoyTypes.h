0b6bda8be19192dc7e21e2a3ac8a8afe1d8df729
Graham Reeves <graham@grahamreeves.com>
2019-11-27 15:36:55 +0000

    Spoke too soon, "bug" in vs means we need an extra c++17 macro check

--- src/SoyTypes.h
+++ src/SoyTypes.h
@@ -376,7 +376,9 @@ inline void Soy::EndianSwap(uint32_t& Value)
 
 //	c++17 functions for pre-c++17 compilers.
 //	standard c++17 definition from https://stackoverflow.com/a/38456243/355753
-#if __cplusplus != 201703L
+//	with visal studio 2017, this is 199711... so extra check
+//	(bug in vs2017) https://stackoverflow.com/questions/38456127/what-is-the-value-of-cplusplus-for-c17/38456243#comment88509472_38456243
+#if __cplusplus != 201703L && !defined(_HAS_CXX17)
 namespace std
 {
 	//	implementation of c++17's std::size() to get safe C-array size
