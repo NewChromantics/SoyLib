6a637e08c66c00b65690df15e2e5d00572607131
Graham Reeves <graham@grahamreeves.com>
2020-04-08 21:16:25 +0100

    Missing IOS stubs for shell execute

---
+++ src/SoyShellExecuteIos.mm
@@ -0,0 +1,9 @@
+#include "SoyShellExecute.h"
+
+
+
+std::shared_ptr<Soy::TProcessInfo> Platform::AllocProcessInfo(const std::string& Command,const ArrayBridge<std::string>& Arguments)
+{
+	Soy_AssertTodo();
+}
+
