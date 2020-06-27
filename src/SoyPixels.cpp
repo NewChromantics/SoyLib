fe2c4b4d24d89a2b4a36f580dd56f9e495d23712
Graham Reeves <graham@grahamreeves.com>
2020-06-27 22:01:34 +0100

    Was overwriting pixel merge maps with old yuv changes

--- src/SoyPixels.cpp
+++ src/SoyPixels.cpp
@@ -273,17 +273,8 @@ const std::map<SoyPixelsFormat::Type,BufferArray<SoyPixelsFormat::Type,2>>& SoyP
 	if ( Map.empty() )
 	{
 		Map[Yuv_8_88].PushBackArray( { Luma, ChromaUV_88 } );
-		Map[Yuv_8_88].PushBackArray( { Luma, ChromaUV_88 } );
-		Map[Yuv_8_88].PushBackArray( { Luma, ChromaUV_88 } );
-
 		Map[Yuv_8_8_8].PushBackArray( { Luma, ChromaUV_8_8 } );
-		Map[Yuv_8_8_8].PushBackArray( { Luma, ChromaUV_8_8 } );
-		Map[Yuv_8_8_8].PushBackArray( { Luma, ChromaUV_8_8 } );
-
 		Map[Yuv_8_88].PushBackArray( { Luma, ChromaUV_88 } );
-		Map[Yuv_8_88].PushBackArray( { Luma, ChromaUV_88 } );
-		Map[Yuv_8_88].PushBackArray( { Luma, ChromaUV_88 } );
-
 		Map[ChromaUV_8_8].PushBackArray( { ChromaU_8, ChromaV_8 } );
 
 		//	the merge is used to match in both directions reverse, so this whole map might need to change?
